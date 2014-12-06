bb2a2da0fb286dca16df55e6228c106318bbc3f1
rourk3 <rourke@unseen.is>
2014-12-06 20:52:26 +0300

    Println some values

---
+++ values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+     fmt.Println("Go" + "language")
+     fmt.Println("24 + 42 =", 24+42)
+     fmt.Println("7.0/2.0 =", 7.0/2.0)
+
+     fmt.Println(true && false)
+     fmt.Println(true || false)
+     fmt.Println(!true)
+}
