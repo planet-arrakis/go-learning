2304f271d2fbb7654995959a2f7ef326d432d3de
rourk3 <rourke@unseen.is>
2014-12-07 20:08:39 +0300

    real Arrays

--- arrays.go
+++ arrays.go
@@ -4,19 +4,23 @@ import "fmt"
 
 func main() {
 
-     s := make([]string, 3)
-     fmt.Println("emp:", s)
+     var a [5]int
+     fmt.Println("emp:", a)
 
-     s[0] = "a"
-     s[1] = "b"
-     s[2] = "c"
+     a[4] = 100
+     fmt.Println("set:", a)
+     fmt.Println("get:", a[4])
 
-     fmt.Println("set:", s)
-     fmt.Println("get:", s[1])
-     fmt.Println("len:", len(s))
+     fmt.Println("len:", len(a))
 
-     s = append(s, "d")
-     s = append(s, "e")
-     fmt.Println("apd:", s)
+     b := [5]int{1, 2, 3, 4, 5}
+     fmt.Println("dcl:", b)
 
+     var twoD [2][3]int
+     for i := 0; i < 2; i++ {
+     	 for j := 0; j < 3; j++ {
+	     twoD[i][j] = i + j
+	 }
+     }
+     fmt.Println("2d:", twoD)
 }
