0b1a5a19cc950ef6245b30068221c1fa18da30fa
Artem T <rourke@unseen.is>
2014-12-15 09:28:52 +0300

    Chanel buffering

---
+++ channel_buffering.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+
+    messages := make(chan string, 2)
+
+    messages <- "buffered"
+    messages <- "channel"
+
+    fmt.Println(<-messages)
+    fmt.Println(<-messages)
+}
